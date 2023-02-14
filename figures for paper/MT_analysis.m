ea039d4fbff6e93c11be2c4758b92966b9ba40b1
QIL123 <125256361+QIL123@users.noreply.github.com>
2023-02-14 14:11:20 +0200

    Delete figures for paper directory

--- figures for paper/MT_analysis.m
+++
@@ -1,9 +0,0 @@
-load MT_data.mat;
-T=MT_table.T;
-M=MT_table.MAt500Oe;
-red='#e34a33';
-fig=figure;
-plot(T,M,'Color',red,'LineWidth',2)
-xlabel('$T (K)$','Interpreter','latex','FontSize',16)
-ylabel('$M (emu/cm^3)$','Interpreter','latex','FontSize',16)
-set(fig,'Color','w')
